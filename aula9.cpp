5de014f5f7b3304098deb71e0df9790182aa9436
Gutool <41220108+Gutool@users.noreply.github.com>
2018-07-14 00:08:19 -0300

    Add files via upload

---
+++ aula9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	int num=10;
+	int num2=50;
+	int sol=0;
+	char opc='s';
+	
+	if (sol)
+	{
+		cout<<"Vouu ao clube";
+	}
+	else
+	{
+		cout<<"vou ao cinema";
+	}
+	return 0;
+}
